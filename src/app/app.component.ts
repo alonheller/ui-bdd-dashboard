69a394609596ab46a37a3ac804c6cdf534c00fd3
alonh <alon.heller@nokia.com>
2016-11-09 15:42:20 +0200

    bar chart added with last 5 results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,9 +22,7 @@ export class AppComponent implements OnInit {
   }
 
 
-  drawChart(data) {
-    this.test = data;
-
+  drawChart(data:Array<ReportResult>) {
     Highcharts.chart('container', {
       chart: {
         plotBackgroundColor: null,
@@ -82,13 +80,96 @@ export class AppComponent implements OnInit {
         }]
       }]
     });
+  }
 
+  drawLast5Chart(data) {
+    Highcharts.chart('container2', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Last 5 BDD'
+      },
+      xAxis: {
+        categories: [data[0].timestamp.toLocaleTimeString("en-us", {
+          weekday: "long", year: "numeric", month: "short",
+          day: "numeric", hour: "2-digit", minute: "2-digit"
+        }), data[1].timestamp.toLocaleTimeString("en-us", {
+          weekday: "long", year: "numeric", month: "short",
+          day: "numeric", hour: "2-digit", minute: "2-digit"
+        }), data[2].timestamp.toLocaleTimeString("en-us", {
+          weekday: "long", year: "numeric", month: "short",
+          day: "numeric", hour: "2-digit", minute: "2-digit"
+        }), data[3].timestamp.toLocaleTimeString("en-us", {
+          weekday: "long", year: "numeric", month: "short",
+          day: "numeric", hour: "2-digit", minute: "2-digit"
+        }), data[4].timestamp.toLocaleTimeString("en-us", {
+          weekday: "long", year: "numeric", month: "short",
+          day: "numeric", hour: "2-digit", minute: "2-digit"
+        })]
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Last 5 BDDs'
+        },
+        stackLabels: {
+          enabled: true,
+          style: {
+            fontWeight: 'bold',
+            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+          }
+        }
+      },
+      legend: {
+        align: 'right',
+        x: -30,
+        verticalAlign: 'top',
+        y: 25,
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+      },
+      plotOptions: {
+        column: {
+          stacking: 'normal',
+          dataLabels: {
+            enabled: true,
+            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+          }
+        }
+      },
+      series: [{
+        name: 'Success',
+        data: [
+          data[0].tests - data[0].errors - data[0].disabled - data[0].failures - data[0].skipped,
+          data[1].tests - data[1].errors - data[1].disabled - data[1].failures - data[1].skipped,
+          data[2].tests - data[2].errors - data[2].disabled - data[2].failures - data[2].skipped,
+          data[3].tests - data[3].errors - data[3].disabled - data[3].failures - data[3].skipped,
+          data[4].tests - data[4].errors - data[4].disabled - data[4].failures - data[4].skipped,
+      ],
+        color: '#00CC00'
+      }, {
+        name: 'Failed',
+        data: [
+          data[0].failures + data[0].errors,
+          data[1].failures + data[1].errors,
+          data[2].failures + data[2].errors,
+          data[3].failures + data[3].errors,
+          data[4].failures + data[4].errors
+        ],
+        color: '#ff0814'
+      }]
+    });
   }
 
   ngOnInit() {
     this.subscription = this.service.getData().subscribe({
       next: (res) => {
-        this.drawChart(res);
+        //this.drawChart(res[0]);
+        this.drawLast5Chart(res);
       }
     });
   }
--- src/app/services/jenkins-bdd-parser.service.ts
+++ src/app/services/jenkins-bdd-parser.service.ts
@@ -1,24 +1,26 @@
 import {Injectable}    from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/observable/bindNodeCallback';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/distinct';
+import 'rxjs/Rx';
 import {parseString} from 'xml2js';
 import {ReportResult} from "../types/ReportResult";
 
 @Injectable()
 export class JenkinsBddParserService {
 
-  private url: string;
+  private baseUrl: string = '/assets/';
+  private fileExtension: string = '.xml';
+  private test1: string = `${this.baseUrl}1${this.fileExtension}`;
+  private test2: string = `${this.baseUrl}2${this.fileExtension}`;
+  private test3: string = `${this.baseUrl}3${this.fileExtension}`;
+  private test4: string = `${this.baseUrl}4${this.fileExtension}`;
+  private test5: string = `${this.baseUrl}5${this.fileExtension}`;
 
   constructor(private http: Http) {
-    this.url = '/assets/1.xml';
+
   }
 
-  getData(): Observable<ReportResult> {
+  /*getData(): Observable<ReportResult> {
     // parse string is a function in xml2js library
     // it works with callback
     // therefore we need to use Observable.bindNodeCallback
@@ -26,16 +28,50 @@ export class JenkinsBddParserService {
 
     return Observable
       .interval(1000)
-      .switchMap(counter => this.http.get(this.url))
+      .switchMap(counter => this.http.get(this.test1))
       .switchMap(res => parseObservable(res._body))
       .map((res: any) => {
         let temp = res.testsuites.testsuite[0].$;
         let reportResult = new ReportResult(temp.name, temp.disabled, temp.errors, temp.failures, temp.hostname, temp.skipped, temp.tests, temp.time, temp.timestamp);
         return reportResult;
       })
-      .distinct(function(x,y) {
+      .distinct(function (x, y) {
         // no need to update the subscribers if it's the same date
         return x.timestamp.valueOf() == y.timestamp.valueOf();
       });
+  }*/
+
+  getData(): Observable<Array<ReportResult>> {
+    // parse string is a function in xml2js library
+    // it works with callback
+    // therefore we need to use Observable.bindNodeCallback
+    let parseObservable = Observable.bindNodeCallback(parseString);
+
+    return Observable
+      /*.interval(5000)*/
+      .zip(
+        this.http.get(this.test1).map((res:any) => parseObservable(res._body)),
+        this.http.get(this.test2).map((res:any) => parseObservable(res._body)),
+        this.http.get(this.test3).map((res:any) => parseObservable(res._body)),
+        this.http.get(this.test4).map((res:any) => parseObservable(res._body)),
+        this.http.get(this.test5).map((res:any) => parseObservable(res._body))
+      )
+      .switchMap(res => {
+        return Observable.zip(...res);
+      })
+      .map((res: Array<any>) => {
+        var items: ReportResult[] = [];
+        for(var i = 0; i < res.length; i++){
+          let temp = res[i].testsuites.testsuite[0].$;
+          let reportResult = new ReportResult(temp.name, temp.disabled, temp.errors, temp.failures, temp.hostname, temp.skipped, temp.tests, temp.time, temp.timestamp);
+          items.push(reportResult);
+        }
+        return items;
+      })
+     /* .distinct(function (x:Array<ReportResult>, y:Array<ReportResult>) {
+        // no need to update the subscribers if it's the same date
+        return x[0].timestamp.valueOf() == y[0].timestamp.valueOf();
+      })*/;
+
   }
 }
